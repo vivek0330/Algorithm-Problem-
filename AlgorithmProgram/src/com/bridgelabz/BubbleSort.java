3172e9d5dd26da974d1d6d9d1189cc57c0923e17
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-25 16:55:04 +0530

    Add: bubble sort method to sort array

---
+++ AlgorithmProgram/src/com/bridgelabz/BubbleSort.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+//Java program for implementation of Bubble Sort
+class BubbleSort {
+	void bubbleSort(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++)
+			for (int j = 0; j < n - i - 1; j++)
+				if (arr[j] > arr[j + 1]) {
+					// swap arr[j+1] and arr[j]
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+	}
+
+	/* Prints the array */
+	void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	// Driver method to test above
+	public static void main(String args[]) {
+		BubbleSort ob = new BubbleSort();
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+		System.out.println("Before Bubble Sort");
+		ob.printArray(arr);
+		ob.bubbleSort(arr);
+		System.out.println("after bubble sort");
+		ob.printArray(arr);
+	}
+}
